8a002dfbe744df9d6f118072af359654b5b4f307
hasanownagh3254-dev <hasanownagh3254@gmail.com>
2026-08-22 12:47:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ import telebot
 import requests
 from flask import Flask
 
-TELEGRAM_TOKEN = "8764118938:AAERKImEtZ5zT2JYFLmIBGSNOg5ynSQP4CI"
-GEMINI_API_KEY = "AQ.Ab8RN6IozEpYOYGMdGE4L1C0dxKPgwQ8jTrgttLYfM_INGHWDg"
+# دریافت توکن‌ها از متغیرهای محیطی سرور
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 app = Flask(__name__)
